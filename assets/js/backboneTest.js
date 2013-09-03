dea6cd5671396c6dce8600739d381b323aabd76d
xin.shux <xin.shux@taobao.com>
2013-09-03 18:23:05 +0800

    tmp

--- assets/js/backboneTest.js
+++ assets/js/backboneTest.js
@@ -1,3 +1,4 @@
+/*
 var Todo = Backbone.Model.extend({
  defaults: {
     title: '',
@@ -22,8 +23,8 @@ var TodoView = Backbone.View.extend({
     todoTpl:  _.template($('#item-template').html()),
 
     events: {
-        'dblclick label' : 'edit',
-        'keypress .edit' : 'updateOnEnter',
+        'dblclick label': 'edit',
+        'keypress .edit': 'updateOnEnter',
         'blur .edit': 'close'
     },
 
@@ -51,3 +52,77 @@ var TodoView = Backbone.View.extend({
 });
 
 var todoView = new TodoView({model: myTodo});
+*/
+var Person = Backbone.Model.extend({
+        defaults: {
+            name: 'emptyDIV'
+        },
+        initialize: function(){
+            console.log('Person has been initiallized')
+        }
+
+    });
+var PersonList = Backbone.Collection.extend({
+      model : Person,
+      nextOrder : function(){
+        if (!this.length) return 1;
+        return this.last().get('id') + 1;
+    },
+    initialize: function(){
+        this.on('add',function(){
+            console.log('add another obj')
+        });
+    }
+});
+
+var personList =new PersonList();
+
+
+var ContentView = Backbone.View.extend({
+    initialize: function(){
+        //alert('contentView has been initialized');
+        this.listenTo(personList,'add', this.resetAll)
+        this.container = this.$(".J_content");
+
+    },
+    el: $('#J_container'),
+
+    template: _.template($('#item-template').html()),
+    
+    render: function(){
+        this.$el.html(this.template);
+    },
+/*
+    render: function(){
+        var template = _.template($('#item-template').html());
+        $(this.el).html(template);
+
+    },
+*/
+    events: {
+        'click #J_add': 'doAdd',
+        'click .show': 'showAlert'
+
+    },
+    doAdd: function(){
+        var person =new Person();
+        personList.add(person);
+    },
+    addOne: function(model){
+        this.container.append(this.template(model.toJSON()));
+    },
+    resetAll: function(){
+        var self = this
+        this.container.html("");
+        _.each(personList.models,function(item){
+          self.container.append(self.template(item.toJSON()));
+        }) 
+    },
+    showAlert: function(e){
+        console.log($(e.currentTarget)[0])
+    }
+});
+
+view =new ContentView();
+
+
