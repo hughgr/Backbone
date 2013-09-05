f8a79605a3441d8e972be2007f533caccd5c84d2
xin.shux <xin.shux@taobao.com>
2013-09-05 21:17:27 +0800

    add new

--- assets/js/backboneTest.js
+++ assets/js/backboneTest.js
@@ -53,31 +53,150 @@ var TodoView = Backbone.View.extend({
 
 var todoView = new TodoView({model: myTodo});
 */
+$.ajaxSetup({
+        url: 'newData.json',
+        dataType: 'json',
+        traditional: true,
+        error: function(){alert('请求异常，请重试')
+    }
+})
+var fetchData = function(callBack,arg1){
+    $.ajax({
+            type: 'get',
+            success: callBack
+        })
+};
+var delData = function(model,callBack){
+    $.ajax({
+            type: 'post',
+            data: model,
+            success: callBack
+        })
+};
+var saveData = function(model,callBack){
+    $.ajax({
+            type: 'post',
+            data: model,
+            success: callBack
+        })
+
+};
+
+var modifyData = function(model,callBack){
+    $.ajax({
+            type: 'post',
+            data: model,
+            success: callBack
+        })
+
+};
 var Person = Backbone.Model.extend({
-        defaults: {
-            name: 'emptyDIV'
+        defaults: function(){
+            return {
+            name: 'emptyDIV',
+            status: [
+                {health: 'ok'},
+                {health: 'bad'},
+                {health: 'soso'}
+            ],
+            homeWork: 'emptyHomeWork'
+        };
+            
         },
+        url: 'newData.json',
         initialize: function(){
             console.log('Person has been initiallized')
-        }
+        },
 
     });
 var PersonList = Backbone.Collection.extend({
       model : Person,
       nextOrder : function(){
         if (!this.length) return 1;
-        return this.last().get('id') + 1;
+        return this.last().get('order') + 1;
     },
     initialize: function(){
         this.on('add',function(){
             console.log('add another obj')
         });
-    }
+    },
+
+});
+var personList = new PersonList();
+
+
+var GroupView = Backbone.View.extend({
+
+        tagName: 'div',
+        TPL: _.template($('#group-template').html()),
+        events: {
+            'click .remove': 'clear',
+            'click .modify': 'modify'
+        },
+        initialize: function(){
+            this.listenTo(this.model,'destory',function(){this.remove();personList.remove(this.model)});
+            //this.listenTo(this.model,'destory',this.remove);
+            this.listenTo(this.model,'change',this.render);
+        },
+        render: function(){
+            this.$el.html(this.TPL(this.model.toJSON()));
+            return this;
+        },
+        modify: function(){
+            var tmpVal = this.model.get('homeWork');
+            this.model.set('homeWork','ppp');
+        },
+        clear: function(){
+            /*this.model.destroy();*/
+            console.log('deldata: '+JSON.stringify(this.model));
+            var self = this;
+            delData( JSON.stringify(this.model), function(){
+                    self.model.trigger('destory');console.log('leftData:'+JSON.stringify(personList))
+                })
+        },
 });
 
-var personList =new PersonList();
+
+var ContainerView = Backbone.View.extend({
+    el: $('#J_container'),
+    events: {
+        'click #J_add' : 'doAdd',
+        'click .remove' : 'doRemove'
+    },
+    addOne: function(person){
+            
+        var groupView = new GroupView({model:person});
+        this.groupContent.append(groupView.render().el)
+    },
+    reset: function(){
+
+        
+    },
+     initialize: function(){
+         this.listenTo(personList,'add',this.addOne);
+         this.listenTo(personList,'all',this.render)
+         this.groupContent = $('#groupContent');
+         
+    },
+    doAdd: function(){
+        fetchData(this.callBack)
+    },
+
+    callBack: function(data){
+        $.each(data,function(i){
+                personList.add(data[i])
+            })
+        return true;
+    },
+    render: function(){
+        //this.groupContent.hide();
+    }
+    });
+    
+var containerView = new ContainerView();
 
 
+/*
 var ContentView = Backbone.View.extend({
     initialize: function(){
         //alert('contentView has been initialized');
@@ -92,13 +211,11 @@ var ContentView = Backbone.View.extend({
     render: function(){
         this.$el.html(this.template);
     },
-/*
     render: function(){
         var template = _.template($('#item-template').html());
         $(this.el).html(template);
 
     },
-*/
     events: {
         'click #J_add': 'doAdd',
         'click .show': 'showAlert'
@@ -125,4 +242,4 @@ var ContentView = Backbone.View.extend({
 
 view =new ContentView();
 
-
+*/
